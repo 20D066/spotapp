49043aec4f36dddfc50a89ba1945af7c0608e366
20D066 <92702729+20D066@users.noreply.github.com>
2022-01-02 23:46:36 +0900

    Update env.js

--- assets/env.js
+++ assets/env.js
@@ -1,2 +1,2 @@
-SSID='1plUyDhVaXn5ilDlFjtQ_SAG9t3xOUEAAZWx89SmeO3Q'
+SSID=https://sheets.googleapis.com/v4/spreadsheets/1EYm0z8UyRYvwyd2LG9UNLI1ct_1IOGdZPUEcEW8WEZU/values/okazaki!A2:K1000?key=AIzaSyD_IOHRq23QuvGfO-N_LYKDyDs9-27v1ek
 SNAME=okazaki!A2:K1000
